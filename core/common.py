fc2f231850b08a702ff016d55eac383b8281999f
Puncher1 <65789180+Puncher1@users.noreply.github.com>
2021-11-13 16:25:18 +0100

    Removed unneeded roles

--- core/common.py
+++ core/common.py
@@ -468,6 +468,7 @@ class Others:
 
 CHHelperRoles = {
     "Essay Reviser": 854135371507171369,
+
     "English": 862160296914714645,
     "English Language": 862160080896000010,
     "English Literature": 862160567921541171,
@@ -499,19 +500,10 @@ CHHelperRoles = {
     "Korean": 862143319458316298,
     "Spanish": 856704808807563294,
 
-    "Chat": 811416051144458250, 
-    "Comp Science": 862160355622780969,
+    "Computer Science": 862160355622780969,
     "Fine Arts":  862160360626716733,
     "Research": 862159906148450314,
-    "SAT/ACT": 862159736384258048,
-  
-    "Essay Reviewer": 854135371507171369,
-    "Language Helper": 862160078370635796,
-    "Science Helper": 862160358264274944,
-    "Math Helper": 862160874214129705,
-    "Social Studies Helper": 862160071466811412,
-
-    "CH_ROLE": 811416051144458250
+    "SAT/ACT": 862159736384258048
 }
 
 
