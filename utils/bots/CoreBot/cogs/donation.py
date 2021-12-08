e3bab037e65fc32b1d5d92fdbd63e317b2a0169a
Puncher1 <65789180+Puncher1@users.noreply.github.com>
2021-12-08 19:17:17 +0100

    Finished donation command

--- utils/bots/CoreBot/cogs/donation.py
+++ utils/bots/CoreBot/cogs/donation.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from core.common import hexColors
 
 class Donation(commands.Cog):
 
@@ -10,7 +11,14 @@ class Donation(commands.Cog):
     @commands.cooldown(1, 10, commands.BucketType.guild)
     async def donate(self, ctx: commands.Context):
         if ctx.author.id == 305354423801217025:
-            await ctx.send("Donation linke here")
+            embedDonate = discord.Embed(
+                color=hexColors.ss_blurple,
+                title=f"Donate",
+                description=f"We do not charge anything for our services, so we rely on your donations! Please consider donating to us."
+                            f"\n\n**Donate here: https://schoolsimplified.org/donate**"
+            )
+            embedDonate.set_footer(text="Great thanks to all our donors!")
+            await ctx.send(embed=embedDonate)
 
 def setup(bot):
     bot.add_cog(Donation(bot))
