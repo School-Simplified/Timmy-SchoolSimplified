03c8c4b64ffbd09161c2c245f2800f4c87d05453
Puncher1 <65789180+Puncher1@users.noreply.github.com>
2021-12-10 23:19:07 +0100

    Removed if statement

--- utils/bots/CoreBot/cogs/donation.py
+++ utils/bots/CoreBot/cogs/donation.py
@@ -10,20 +10,19 @@ class Donation(commands.Cog):
     @commands.command(aliases=["donation"])
     @commands.cooldown(1, 10, commands.BucketType.guild)
     async def donate(self, ctx: commands.Context):
-        if ctx.author.id == 305354423801217025:
-            timmyDonation_png = discord.File(Others.timmyDonation_path, filename=Others.timmyDonation_png)
+        timmyDonation_png = discord.File(Others.timmyDonation_path, filename=Others.timmyDonation_png)
 
-            embedDonate = discord.Embed(
-                color=hexColors.ss_blurple,
-                title=f"Donate",
-                description=f"Thank you for your generosity in donating to School Simplified. "
-                            f"We do not charge anything for our services, and your support helps to further our mission "
-                            f"to *empower the next generation to revolutionize the future through learning*."
-                            f"\n\n**Donate here: https://schoolsimplified.org/donate**"
-            )
-            embedDonate.set_footer(text="Great thanks to all our donors!")
-            embedDonate.set_thumbnail(url=f"attachment://{Others.timmyDonation_png}")
-            await ctx.send(embed=embedDonate, file=timmyDonation_png)
+        embedDonate = discord.Embed(
+            color=hexColors.ss_blurple,
+            title=f"Donate",
+            description=f"Thank you for your generosity in donating to School Simplified. "
+                        f"We do not charge anything for our services, and your support helps to further our mission "
+                        f"to *empower the next generation to revolutionize the future through learning*."
+                        f"\n\n**Donate here: https://schoolsimplified.org/donate**"
+        )
+        embedDonate.set_footer(text="Great thanks to all our donors!")
+        embedDonate.set_thumbnail(url=f"attachment://{Others.timmyDonation_png}")
+        await ctx.send(embed=embedDonate, file=timmyDonation_png)
 
 def setup(bot):
     bot.add_cog(Donation(bot))
