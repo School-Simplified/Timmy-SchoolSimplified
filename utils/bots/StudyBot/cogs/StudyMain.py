9b66584aea2c0a02d180c8324119fafd182b39bd
Puncher1 <65789180+Puncher1@users.noreply.github.com>
2021-12-11 23:01:26 +0100

    test

--- utils/bots/StudyBot/cogs/StudyMain.py
+++ utils/bots/StudyBot/cogs/StudyMain.py
@@ -14,6 +14,9 @@ class StudyToDo(commands.Cog):
     @commands.group(aliaseS=["study-todo"])
     async def studytodo(self, ctx: commands.Context):
         if ctx.message.content == "+studytodo":
+            print(ctx.command.parent)
+            print(ctx.command.parent.all_commands)
+            print(ctx.command.parent.walk_commands())
             print(ctx.command.parent.commands)
             subcommands = "/".join(ctx.command.parent.commands)
             signature = f"{ctx.prefix}{ctx.command.qualified_name} <{subcommands}>"
