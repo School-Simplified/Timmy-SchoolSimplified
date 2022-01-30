33a3b02cd1ba322f5ad8b9f91cd52180d2b9aa5c
pandabear189 <88685021+pandabear189@users.noreply.github.com>
2022-01-30 14:07:10 -0600

    email cmd --> slash

--- utils/bots/CoreBot/cogs/GmailDisc.py
+++ utils/bots/CoreBot/cogs/GmailDisc.py
@@ -5,41 +5,48 @@ from core.common import Others, hexColors
 from core.checks import is_botAdmin
 from core.gmailapi import auth, create_message
 from discord.ext import commands
+from discord import Option, slash_command, permissions
+from core.commoon import MAIN_ID
 
 regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
 
+
 def email_check(email):
-    if(re.fullmatch(regex, email)):
+    if re.fullmatch(regex, email):
         return True
 
     else:
         return False
 
+
 class GmailAPI(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @commands.command(aliases=["email"])
-    @commands.cooldown(1, 10, commands.BucketType.user)
-    @is_botAdmin
-    async def send(self, ctx: commands.Context, to: str, subject: str, *, message: str):
+    @slash_command(
+        guild_ids=[MAIN_ID.g_main]
+    )
+    @permissions.has_any_role(
+        "Staff"
+    )
+    async def email(
+            self,
+            ctx: discord.ApplicationContext,
+            to: Option(str, description="Who to send your email to"),
+            subject: Option(str, description="Your email subject"),
+            message: Option(str, description="Your email message")):
         """Send a message to a user via their email.
         """
         if not email_check(to):
             await ctx.send("That is not a valid email address!")
             return
 
-        message = message + f"\nSent from: {ctx.author.name}#{ctx.author.discriminator}"
+        message = message + f"\nSent from: {ctx.author}"
 
         service = auth()
-        message = create_message(service, "timmy@schoolsimplified.org", to, subject, message)
-        await ctx.send("Message sent via gmail!")
+        create_message(service, "timmy@schoolsimplified.org", to, subject, message)
+        await ctx.respond("Message sent via gmail!")
 
-
-        
-
-
-        
 
 def setup(bot):
     bot.add_cog(GmailAPI(bot))
