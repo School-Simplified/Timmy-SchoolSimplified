7285c20aba1860b39ee77129583cfce3d6630c3e
Puncher1 <65789180+Puncher1@users.noreply.github.com>
2022-02-26 18:28:16 +0100

    test print

--- test.py
+++ test.py
@@ -43,7 +43,7 @@ string = "213d 4h 123m"
 timeDict: dict = timeStringConvert(string)
 print(timeDict)
 print(os.path.realpath(__file__))
-runDir = re.search("/home/[a-zA-Z]+", os.path.realpath(__file__))
+runDir = re.search("/home/(\w)+/(\w)+/(\w)+.py", os.path.realpath(__file__))
 
 if runDir is not None:
     print(runDir.group(0))
