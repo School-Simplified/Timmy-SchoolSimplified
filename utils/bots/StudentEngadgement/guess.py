83bfed326173fadc8e245d3871259fc1bf6ffe37
pandabear189 <88685021+pandabear189@users.noreply.github.com>
2022-03-21 20:22:15 -0500

    check decorators for slash cmds

--- utils/bots/StudentEngadgement/guess.py
+++ utils/bots/StudentEngadgement/guess.py
@@ -1,6 +1,8 @@
+from typing import Dict, List, Literal, Union
+
 import discord
-from discord import Option, slash_command, SlashCommandGroup
 from discord.ext import commands
+from discord.app_commands import command, Group
 from core.common import MAIN_ID
 
 
@@ -8,76 +10,87 @@ class Engagement(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @slash_command(
+    @command(
         name="puzzle_guess",
-        guild_ids=[MAIN_ID.g_main],
+        description="Submit a guess for the weekly puzzle!"
     )
-    async def _guess(self, ctx: discord.ApplicationContext, guess: str):
-        """
-        :param guess: The guess you are making to the weekly puzzle
-        """
+    async def _guess(self, interaction: discord.Interaction, guess: str):
         embed = discord.Embed(
             color=0xc387ff,
             title="Puzzle Guess",
             description=f"```{guess}```",
             timestamp=discord.utils.utcnow()
         )
-        embed.set_author(name=ctx.author, icon_url=ctx.author.avatar.url)
+        embed.set_author(name=interaction.user, icon_url=interaction.user.avatar.url)
         ssd: discord.Guild = self.bot.get_guild(778406166735880202)
         guess_channel: discord.TextChannel = ssd.get_channel(950083341967843398)
         await guess_channel.send(embed=embed)
-        await ctx.respond("Your guess has been submitted!", ephemeral=True)
+        await interaction.response.send_message("Your guess has been submitted!", ephemeral=True)
 
-    __suggest = SlashCommandGroup(
-        "suggest",
-        "Suggest something... ",
-        guild_ids=[MAIN_ID.g_main]
-    )
 
-    @__suggest.command()
-    async def book(
+class Suggest(Group):
+    def __init__(self):
+        super().__init__(
+            name="suggest",
+            description="Suggest something for community engagement!",
+            guild_ids=[MAIN_ID.g_main]
+        )
+
+    @command(name="book")
+    async def __book(self, interaction: discord.Interaction):
+        await interaction.response.send_modal(SuggestModal("Book"))
+
+
+class SuggestModal(discord.ui.Modal):
+    def __init__(
             self,
-            ctx: discord.ApplicationContext,
-            title: Option(
-                str,
-                description="Which book are you recommending?"
-            ),
-            rating: Option(
-                int,
-                max_value=10,
-                min_value=0,
-                description="What rating does this book have?"
-            ),
-            description: Option(
-                str,
-                description="Give a short description of the book with no spoilers."
-            ),
-            reason: Option(
-                str,
-                description="Why do you want to recommend this book?"
-            )
+            suggest_type: Literal["Media", "Book",]
     ):
+        super().__init__(
+            timeout=None,
+            title=suggest_type + "Suggestion"
+        )
+        self.type = suggest_type
+        self.book_question_list: List[Dict[str, Union[str, bool]]] = [
+            {
+                "question": "Which book are you recommending?",
+                "required": True
+            },
+            {
+                "question": "What rating does this book have?",
+                "required": True
+            },
+            {
+                "question": "Give a short description of the book with no spoilers.",
+                "required": True
+            },
+            {
+                "question": "Why do you want to recommend this book?",
+                "required": False
+            },
+        ]
+
+        self.type_to_questions_list = {
+            "Book": self.book_question_list
+        }
+        for question in self.type_to_questions_list[self.type]:
+            self.add_item(
+                discord.ui.TextInput(
+                    label=question["question"],
+                    required=question["required"],
+                    max_length=1024
+                )
+            )
+
+    async def on_submit(self, interaction: discord.Interaction) -> None:
+        await interaction.response.send_message("Submitted!")
         embed = discord.Embed(
-            title=f"Media/Book Suggestion: {title}",
-            description=description
-        )
-        embed.add_field(
-            name="Short description of the book",
-            value=description[:1024]
-        )
-        embed.add_field(
-            name="Why do you want to recommend this book?",
-            value=reason[:1024]
-        )
-        embed.add_field(
-            name="Rating",
-            value=f"{rating}"
-        )
-        await ctx.respond("Sending suggestion...", ephemeral=True)
 
-        channel = self.bot.get_guild(950799439625355294).get_channel(954190487026274344)
-        await channel.send(embed=embed)
+        )
+        ...
+        # idk what to do here, i want the embed field to be the question and the answer to be the field value
 
 
-def setup(bot):
-    bot.add_cog(Engagement(bot))
+async def setup(bot: commands.Bot):
+    await bot.add_cog(Engagement(bot))
+    bot.tree.add_command(Suggest())
