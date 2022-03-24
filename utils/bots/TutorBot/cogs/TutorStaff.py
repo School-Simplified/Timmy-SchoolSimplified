c37070bed10e2da15905ab9bd9c33c32da95ea20
pandabear189 <88685021+pandabear189@users.noreply.github.com>
2022-03-24 09:34:33 -0500

    tutor staff cmds to dpy

--- utils/bots/TutorBot/cogs/TutorStaff.py
+++ utils/bots/TutorBot/cogs/TutorStaff.py
@@ -1,14 +1,15 @@
 import random
 import string
 from datetime import datetime, timedelta
+from typing import Literal
+
 import pytz
 
 import discord
 from core import database
 from core.common import MAIN_ID, TUT_ID
+from discord.app_commands import command, describe, guilds
 from discord.ext import commands
-from discord import Option, slash_command
-from discord.commands import permissions
 
 
 async def id_generator(size=3, chars=string.ascii_uppercase):
@@ -31,20 +32,26 @@ class TutorBotStaffCMD(commands.Cog):
     @slash_command(
         name="schedule",
         description="Create a Tutor Session",
-        guild_ids=[MAIN_ID.g_main],
     )  # SLASH CMD FOR MAIN SERVER
-    @permissions.has_any_role("Tutor")
+    @guilds(MAIN_ID.g_main)
+    # @permissions.has_any_role("Tutor")
+    @describe(
+        date="Enter a date in MM/DD format. EX: 02/02",
+        time="Enter a time in HH:MM format in EST. EX: 3:00",
+        ampm="AM or PM",
+        student="Enter the student ID you'll be tutoring for this session.",
+        repeats="Does your Tutoring Session repeat?",
+        subject="Tutoring subject"
+    )
     async def schedule(
         self,
-        ctx,
-        date: Option(str, "Enter a date in MM/DD format. EX: 02/02"),
-        time: Option(str, "Enter a time in HH:MM format in EST. EX: 3:00"),
-        ampm: Option(str, "AM or PM", choices=["AM", "PM"]),
-        student: Option(
-            discord.User, "Enter the student you'll be tutoring for this session."
-        ),
-        repeats: Option(bool, "Does your Tutoring Session repeat?"),
-        subject: Option(str, "Tutoring Subject"),
+        interaction: discord.Interaction,
+        date: str,
+        time: str,
+        ampm: Literal["AM", "PM"],
+        student: discord.Member,
+        repeats: bool,
+        subject: str,
     ):
         embed = discord.Embed(
             title="Schedule Confirmed",
@@ -67,7 +74,7 @@ class TutorBotStaffCMD(commands.Cog):
                 name="Values",
                 value=f"**Session ID:** `{session_id}`"
                 f"\n**Student:** `{student.name}`"
-                f"\n**Tutor:** `{ctx.author.name}`"
+                f"\n**Tutor:** `{interaction.user.name}`"
                 f"\n**Date:** <t:{timestamp}:d>"
                 f"\n**Time:** <t:{timestamp}:t>"
                 f"\n**Repeat?:** `{repeats}`",
@@ -78,13 +85,13 @@ class TutorBotStaffCMD(commands.Cog):
                 Date=datetime_session,
                 Time=time,
                 StudentID=student.id,
-                TutorID=ctx.author.id,
+                TutorID=interaction.user.id,
                 Repeat=repeats,
                 Subject=subject,
                 ReminderSet=False,
             )
             query.save()
-            await ctx.respond(embed=embed)
+            await interaction.response.send_message(embed=embed)
         else:
             embed = discord.Embed(
                 title="Failed to Generate Session",
@@ -92,7 +99,7 @@ class TutorBotStaffCMD(commands.Cog):
                 f"sessions.",
                 color=discord.Color.red(),
             )
-            await ctx.respond(embed=embed)
+            await interaction.response.send_message(embed=embed)
 
     @commands.command(name="schedule")
     @commands.has_any_role("Tutor")
@@ -155,22 +162,28 @@ class TutorBotStaffCMD(commands.Cog):
             )
             await ctx.send(embed=embed)
 
-    @slash_command(
+    @command(
         name="schedule",
         description="Create a Tutor Session",
-        guild_ids=[TUT_ID.g_tut],
-    )  # SLASH CMD FOR TUTOR SERVER
+    )
+    @guilds(TUT_ID.g_tut)
+    @describe(
+        date="Enter a date in MM/DD format. EX: 02/02",
+        time="Enter a time in HH:MM format in EST. EX: 3:00",
+        ampm="AM or PM",
+        student="Enter the student ID you'll be tutoring for this session.",
+        repeats="Does your Tutoring Session repeat?",
+        subject="Tutoring subject"
+    )
     async def schedule_t(
         self,
-        ctx,
-        date: Option(str, "Enter a date in MM/DD format. EX: 02/02"),
-        time: Option(str, "Enter a time in HH:MM format in EST. EX: 3:00"),
-        ampm: Option(str, "AM or PM", choices=["AM", "PM"]),
-        student: Option(
-            str, "Enter the student ID you'll be tutoring for this session."
-        ),
-        repeats: Option(bool, "Does your Tutoring Session repeat?"),
-        subject: Option(str, "Tutoring Subject"),
+        interaction: discord.Interaction,
+        date: str,
+        time: str,
+        ampm: Literal["AM", "PM"],
+        student: str,
+        repeats: bool,
+        subject: str,
     ):
         embed = discord.Embed(
             title="Schedule Confirmed",
@@ -198,7 +211,7 @@ class TutorBotStaffCMD(commands.Cog):
                 name="Values",
                 value=f"**Session ID:** `{session_id}`"
                 f"\n**Student:** `{student.name}`"
-                f"\n**Tutor:** `{ctx.author.name}`"
+                f"\n**Tutor:** `{interaction.user.name}`"
                 f"\n**Date:** <t:{timestamp}:d>"
                 f"\n**Time:** <t:{timestamp}:t>"
                 f"\n**Repeat?:** `{repeats}`",
@@ -209,13 +222,13 @@ class TutorBotStaffCMD(commands.Cog):
                 Date=datetime_session,
                 Time=time,
                 StudentID=student.id,
-                TutorID=ctx.author.id,
+                TutorID=interaction.user.id,
                 Repeat=repeats,
                 Subject=subject,
                 ReminderSet=False,
             )
             query.save()
-            await ctx.respond(embed=embed)
+            await interaction.response.send_message(embed=embed)
         else:
             embed = discord.Embed(
                 title="Failed to Generate Session",
@@ -223,20 +236,20 @@ class TutorBotStaffCMD(commands.Cog):
                 f"sessions.",
                 color=discord.Color.red(),
             )
-            await ctx.respond(embed=embed)
+            await interaction.response.send_message(embed=embed)
 
-    @slash_command(
+    @command(
         name="skip",
         description="Skip a tutoring session",
-        guild_ids=[MAIN_ID.g_main, TUT_ID.g_tut],
     )
-    async def skip(self, ctx, id: str):
+    @guilds(MAIN_ID.g_main, TUT_ID.g_tut)
+    async def skip(self, interaction: discord.Interaction, id: str):
         query: database.TutorBot_Sessions = database.TutorBot_Sessions.select().where(
             database.TutorBot_Sessions.SessionID == id
         )
         if query.exists():
             query = query.get()
-            if query.TutorID == ctx.author.id:
+            if query.TutorID == interaction.user.id:
 
                 old = query.Date
                 new = timedelta(days=7)
@@ -247,14 +260,14 @@ class TutorBotStaffCMD(commands.Cog):
                 query.Date = nextweek
                 query.save()
 
-                await ctx.respond(f"Re-scheduled Session to `{nw}`")
+                await interaction.response.send_message(f"Re-scheduled Session to `{nw}`")
             else:
                 embed = discord.Embed(
                     title="Invalid Permissions",
                     description="This session does exist, but you are not the owner of it!",
                     color=discord.Color.red(),
                 )
-                await ctx.respond(embed=embed)
+                await interaction.response.send_message(embed=embed)
 
         else:
             embed = discord.Embed(
@@ -262,20 +275,20 @@ class TutorBotStaffCMD(commands.Cog):
                 description="This session does not exist, please check the ID you've provided!",
                 color=discord.Color.red(),
             )
-            await ctx.respond(embed=embed)
+            await interaction.response.send_message(embed=embed)
 
-    @slash_command(
+    @command(
         name="remove",
-        description="Remove a tutoring session",
-        guild_ids=[MAIN_ID.g_main, TUT_ID.g_tut],
+        description="Remove a tutoring session"
     )
-    async def remove(self, ctx, id):
+    @guilds(MAIN_ID.g_main, TUT_ID.g_tut)
+    async def remove(self, interaction: discord.Interaction, id: str):
         query = database.TutorBot_Sessions.select().where(
             database.TutorBot_Sessions.SessionID == id
         )
         if query.exists():
             query = query.get()
-            if query.TutorID == ctx.author.id:
+            if query.TutorID == interaction.user.id:
                 query.delete_instance()
 
                 embed = discord.Embed(
@@ -288,38 +301,38 @@ class TutorBotStaffCMD(commands.Cog):
                     value=f"**Session ID:** `{query.SessionID}`"
                     f"\n**Student ID:** `{query.StudentID}`",
                 )
-                await ctx.respond(embed=embed)
+                await interaction.response.send_message(embed=embed)
             else:
                 embed = discord.Embed(
                     title="Invalid Permissions",
                     description="This session does exist, but you are not the owner of it!",
                     color=discord.Color.red(),
                 )
-            await ctx.respond(embed=embed)
+            await interaction.response.send_message(embed=embed)
         else:
             embed = discord.Embed(
                 title="Invalid Session",
                 description="This session does not exist, please check the ID you've provided!",
                 color=discord.Color.red(),
             )
-            await ctx.respond(embed=embed)
+            await interaction.response.send_message(embed=embed)
 
-    @slash_command(
+    @command(
         name="clear",
         description="Clear a tutoring session",
-        guild_ids=[MAIN_ID.g_main, TUT_ID.g_tut],
     )
-    async def clear(self, ctx):
+    @guilds(MAIN_ID.g_main, TUT_ID.g_tut)
+    async def clear(self, interaction: discord.Interaction):
         query = database.TutorBot_Sessions.select().where(
-            database.TutorBot_Sessions.TutorID == ctx.author.id
+            database.TutorBot_Sessions.TutorID == interaction.user.id
         )
         var = query.count()
         if var == 0:
-            await ctx.respond("You don't have any tutor sessions!")
+            await interaction.response.send_message("You don't have any tutor sessions!")
         else:
             for session in query:
                 session.delete_instance()
-            await ctx.respond(
+            await interaction.response.send_message(
                 f"All sessions have been deleted!" f"\nDeleted {var} sessions."
             )
 
