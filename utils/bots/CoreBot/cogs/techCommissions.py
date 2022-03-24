095c36b8232d48fe21a9e1be609d6e3b33c47dd2
Puncher1 <65789180+Puncher1@users.noreply.github.com>
2022-03-24 22:01:24 +0100

    Rewrite techCommssions.py
    events/AutoThreadUnarchive.py got moved and renamed to bots/CoreBot/cogs/techCommissions.py

--- utils/bots/CoreBot/cogs/techCommissions.py
+++ utils/bots/CoreBot/cogs/techCommissions.py
@@ -1,50 +1,47 @@
 import discord
+from discord import ui
 from discord.ext import commands, tasks
 from core.common import TECH_ID
 from core.checks import is_botAdmin
 from core import database
 
 
-class BotRequestModal(discord.ui.Modal):
-    def __init__(self, bot) -> None:
+class BotRequestModal(ui.Modal):
+    def __init__(self, bot: commands.Bot) -> None:
         super().__init__(title="Bot Development Request")
         self.bot = bot
 
-        self.add_item(
-            discord.ui.TextInput(
-                label="What is a descriptive title for your project?",
-                style=discord.TextStyle.long,
-                max_length=1024,
-            )
-        )
-        self.add_item(
-            discord.ui.TextInput(
-                label="Which team is this project for?",
-                style=discord.TextStyle.short,
-            )
-        )
-        self.add_item(
-            discord.ui.TextInput(
-                label="Write a brief description of the project.",
-                style=discord.TextStyle.long,
-                max_length=1024,
-            )
-        )
-        self.add_item(
-            discord.ui.TextInput(
-                label="Do you have approval for this commission?",
-                style=discord.TextStyle.long,
-                max_length=1024,
-            )
-        )
-        self.add_item(
-            discord.ui.TextInput(
-                label="Anything else?",
-                style=discord.TextStyle.long,
-                required=False,
-                max_length=1024,
-            )
-        )
+    titleTI = ui.TextInput(
+        label="What is a descriptive title for your project?",
+        style=discord.TextStyle.long,
+        max_length=1024,
+    )
+
+    teamTI = ui.TextInput(
+        label="Which team is this project for?",
+        style=discord.TextStyle.short,
+        max_length=1024
+    )
+
+    descriptionTI = ui.TextInput(
+        label="Write a brief description of the project.",
+        style=discord.TextStyle.long,
+        max_length=1024,
+    )
+
+    approvalTI = ui.TextInput(
+        label="Do you have approval for this commission?",
+        style=discord.TextStyle.long,
+        max_length=1024,
+    )
+
+    anythingElseTI = ui.TextInput(
+        label="Anything else?",
+        style=discord.TextStyle.long,
+        required=False,
+        max_length=1024,
+    )
+
 
     async def callback(self, interaction: discord.Interaction):
         await interaction.response.send_message(
@@ -58,23 +55,23 @@ class BotRequestModal(discord.ui.Modal):
             name=interaction.user.name, icon_url=interaction.user.avatar.url
         )
         embed.add_field(
-            name="Project Title", value=self.children[0].value, inline=False
+            name="Project Title", value=self.titleTI.value, inline=False
         )
         embed.add_field(
-            name="Team Requester", value=self.children[1].value, inline=False
+            name="Team Requester", value=self.teamTI.value, inline=False
         )
         embed.add_field(
-            name="Project Description", value=self.children[2].value, inline=False
+            name="Project Description", value=self.descriptionTI.value, inline=False
         )
-        embed.add_field(name="Approval", value=self.children[3].value, inline=False)
+        embed.add_field(name="Approval", value=self.approvalTI.value, inline=False)
         embed.add_field(
-            name="Anything else?", value=self.children[4].value, inline=False
+            name="Anything else?", value=self.anythingElseTI.value, inline=False
         )
         embed.set_footer(text="Bot Developer Commission")
 
-        c_ch: discord.TextChannel = await self.bot.fetch_channel(933181562885914724)
+        c_ch: discord.TextChannel = self.bot.get_channel(TECH_ID.ch_botreq)
         msg: discord.Message = await c_ch.send(interaction.user.mention, embed=embed)
-        thread = await msg.create_thread(name=self.children[0].value)
+        thread = await msg.create_thread(name=self.titleTI.value)
 
         await thread.send(
             f"{interaction.user.mention} has requested a bot development project.\n<@&{TECH_ID.r_botDeveloper}>"
@@ -101,14 +98,15 @@ class CommissionTechButton(discord.ui.View):
 class TechProjectCMD(commands.Cog):
     def __init__(self, bot):
         self.bot: commands.Bot = bot
-        self.AUTO_UNARHCIVE.start()
+        self.autoUnarchiveThread.start()
 
     async def cog_unload(self):
-        self.AUTO_UNARHCIVE.cancel()
+        self.autoUnarchiveThread.cancel()
+
 
     @commands.command()
     @is_botAdmin
-    async def techembedc(self, ctx):
+    async def techEmbed(self, ctx):
         embed = discord.Embed(
             title="Bot Developer Commissions", color=discord.Color.green()
         )
@@ -124,58 +122,60 @@ class TechProjectCMD(commands.Cog):
         view = CommissionTechButton(self.bot)
         await ctx.send(embed=embed, view=view)
 
+
     @commands.command()
-    async def closethread(self, ctx: commands.Context):
-        channel: discord.TextChannel = await self.bot.fetch_channel(TECH_ID.ch_botreq)
+    async def closeThread(self, ctx: commands.Context):
+        channel: discord.TextChannel = self.bot.get_channel(TECH_ID.ch_botreq)
         thread: discord.Thread = ctx.channel
+
         if thread in channel.threads:
             await ctx.send("Closed thread!")
             query = database.TechCommissionArchiveLog.create(ThreadID=thread.id)
             query.save()
-            thread.archive()
+
+            await thread.edit(archived=True)
         else:
             await ctx.send("Not a valid thread.")
 
+
     @commands.command()
-    async def openthread(self, ctx: commands.Context):
-        channel: discord.TextChannel = await self.bot.fetch_channel(TECH_ID.ch_botreq)
+    async def openThread(self, ctx: commands.Context):
+        channel: discord.TextChannel = self.bot.get_channel(TECH_ID.ch_botreq)
         thread: discord.Thread = ctx.channel
+
         if thread in channel.threads:
             query = database.TechCommissionArchiveLog.select().where(
-                database.ThreadID == thread.id
+                database.TechCommissionArchiveLog.ThreadID == thread.id
             )
             if query.exists():
                 query.delete_instance()
-                thread.unarchive()
+
+                await thread.edit(archived=False)
                 await ctx.send("Re-opened thread!")
             else:
                 await ctx.send("Unknown Error")
         else:
             await ctx.send("Not a valid thread.")
 
+
     @tasks.loop(seconds=60.0)
-    async def AUTO_UNARHCIVE(self):
+    async def autoUnarchiveThread(self):
         """
-        Create a task loop to make sure threads don't automatically archive due to inactivity.
+        Creates a task loop to make sure threads don't automatically archive due to inactivity.
         """
-        guilds = [[932066545117585428, [933181562885914724]]]
-        channel: discord.TextChannel = await self.bot.fetch_channel(
-            int(TECH_ID.ch_botreq)
-        )
-        # print(channel.name, type(channel))
-        for guild in guilds:
-            guild = await self.bot.fetch_guild(932066545117585428)
-            # print("stuff", guild.threads)
-            for thread in guild.threads:
-                # print(channel.name, type(channel))
-                query = (
-                    database.TechCommissionArchiveLog.select()
-                        .where(database.ThreadID == thread.id)
-                        .exists()
-                )
-                # print(thread.archived, query, (thread.parent_id in guild[1]))
-                if thread.archived and thread.parent_id in guild[1]:
-                    await thread.unarchive()
+
+        guild = TECH_ID.g_tech
+        channel: discord.TextChannel = self.bot.get_channel(TECH_ID.ch_botreq)
+
+        thread = ...    # type: discord.Thread
+        for thread in guild.threads:
+            query = (
+                database.TechCommissionArchiveLog.select().where(database.TechCommissionArchiveLog.ThreadID == thread.id)
+            )
+            if query.exists():
+
+                if thread.archived:
+                    await thread.edit(archived=False)
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(TechProjectCMD(bot))
