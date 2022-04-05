44f18ccecaf1a3738230fb676eadd349e1068af6
SpaceT0 <space@spaceturtle.tech>
2022-04-05 17:10:35 -0400

    Version Change

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ Copyright (C) School Simplified - All Rights Reserved
  * Written by School Simplified, IT Dept. <timmy@schoolsimplified.org>, March 2022
 """
 
-__version__ = "beta3.0.1"
+__version__ = "3.0.1"
 __author__ = "School Simplified, IT Dept."
 __author_email__ = "timmy@schoolsimplified.org"
 
