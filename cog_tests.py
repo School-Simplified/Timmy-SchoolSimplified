ca9a9c604d3bd42809be27f4fc46f5d0fc0b3267
Puncher <mcocexpert@hispeed.ch>
2022-04-19 21:19:49 +0200

    Code Guidelines Rewrite: cog_tests.py

--- cog_tests.py
+++ cog_tests.py
@@ -1,23 +1,23 @@
 import os
 import sys
-from pathlib import Path
 
 import discord
 import discord.ext.test as dpytest
-import pytest
 from discord.ext import commands
+from pathlib import Path
+import pytest
 
 
 def get_extensions():
-    extensions = []
-    extensions.append("jishaku")
+    extensions = ["jishaku"]
     if sys.platform == "win32" or sys.platform == "cygwin":
         dirpath = "\\"
     else:
         dirpath = "/"
 
     for file in Path("utils").glob("**/*.py"):
-        if "!" in file.name or "DEV" in file.name:
+
+        if any(dev_symbol in file.name for dev_symbol in ["!", "DEV"]):
             continue
         extensions.append(str(file).replace(dirpath, ".").replace(".py", ""))
     return extensions
@@ -25,9 +25,9 @@ def get_extensions():
 
 @pytest.fixture
 def bot(event_loop):
-    bot = commands.Bot(
-        command_prefix="/", event_loop=event_loop, intents=discord.Intents.all()
-    )
+    bot = commands.Bot(command_prefix="/",
+                       event_loop=event_loop,
+                       intents=discord.Intents.all())
     bot.remove_command("help")
     dpytest.configure(bot)
     return bot
