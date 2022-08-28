90657ca88d87987d28767c7ebb493952c56ed5cf
Puncher <65789180+Puncher1@users.noreply.github.com>
2022-08-28 14:19:34 +0200

    Add `START_API` flag check

--- utils/events/uptime_poster.py
+++ utils/events/uptime_poster.py
@@ -27,42 +27,43 @@ class MetricPoster(commands.Cog):
         post request with bot.latency.
         Thanks, GitHub copilot <3
         """
-        ping_payload = {
-            "value": round(self.bot.latency * 1000),
-            "timestamp": time.time(),
-        }
-        cpu_payload = {"value": psutil.cpu_percent(), "timestamp": time.time()}
-        ram_payload = {
-            "value": psutil.virtual_memory().percent,
-            "timestamp": time.time(),
-        }
+        if os.getenv("START_API"):
+            ping_payload = {
+                "value": round(self.bot.latency * 1000),
+                "timestamp": time.time(),
+            }
+            cpu_payload = {"value": psutil.cpu_percent(), "timestamp": time.time()}
+            ram_payload = {
+                "value": psutil.virtual_memory().percent,
+                "timestamp": time.time(),
+            }
 
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer " + os.getenv("INSTATUS_TOKEN"),
-        }
-        ping_response = requests.request(
-            "POST",
-            "https://api.instatus.com/v1/cl69luezk0117y5lr0hll9h9y/metrics"
-            "/cl6bb20bt92861xpnd3m5y8o4f",
-            headers=headers,
-            json=ping_payload,
-        )
-        cpu_response = requests.request(
-            "POST",
-            "https://api.instatus.com/v1/cl69luezk0117y5lr0hll9h9y/metrics"
-            "/cl6bb1u6793568windnu7f5hw3",
-            headers=headers,
-            json=cpu_payload,
-        )
-        ram_response = requests.request(
-            "POST",
-            "https://api.instatus.com/v1/cl69luezk0117y5lr0hll9h9y/metrics"
-            "/cl6bb1oae96997x8ndm6hf8gft",
-            headers=headers,
-            json=ram_payload,
-        )
-        _log.info("Posted metrics")
+            headers = {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + os.getenv("INSTATUS_TOKEN"),
+            }
+            ping_response = requests.request(
+                "POST",
+                "https://api.instatus.com/v1/cl69luezk0117y5lr0hll9h9y/metrics"
+                "/cl6bb20bt92861xpnd3m5y8o4f",
+                headers=headers,
+                json=ping_payload,
+            )
+            cpu_response = requests.request(
+                "POST",
+                "https://api.instatus.com/v1/cl69luezk0117y5lr0hll9h9y/metrics"
+                "/cl6bb1u6793568windnu7f5hw3",
+                headers=headers,
+                json=cpu_payload,
+            )
+            ram_response = requests.request(
+                "POST",
+                "https://api.instatus.com/v1/cl69luezk0117y5lr0hll9h9y/metrics"
+                "/cl6bb1oae96997x8ndm6hf8gft",
+                headers=headers,
+                json=ram_payload,
+            )
+            _log.info("Posted metrics")
 
     @instatus_metric_post.before_loop
     async def before_loop_(self):
